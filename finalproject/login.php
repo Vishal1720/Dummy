edf74a29f6787a181b8e259ae384552f212fb872
Vishal <89019986+Vishal1720@users.noreply.github.com>
2024-03-20 15:55:03 +0530

    Update login.php
    added login functionality

--- finalproject/login.php
+++ finalproject/login.php
@@ -1,2 +1,21 @@
 <?php
-require "connection.php";
+$server="localhost";
+$username="root";
+$pass="";
+
+$con=mysqli_connect($server,$username,$pass);
+
+if(!$con)
+{
+    die("Error");
+}
+$name=$_POST['username'];
+$pwd=$_POST['password'];
+
+$query="SELECT * FROM `oes`.`regform` WHERE username='$name' and password='$pwd';";
+
+$res=$con->query($query);
+if($res->num_rows==1)
+Header("location:homepage.html");
+else
+Header("location:login.html");
